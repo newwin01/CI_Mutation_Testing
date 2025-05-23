d7c0ea9a559eb8b4312437d3a93f694a6cae76d7
newwin01 <newwin0198@gmail.com>
2025-05-23 21:18:48 +0900

    Fix escape sequence in string mutation to handle malformed "\\N" correctly, fix #2

--- mutmut/mutmut/node_mutation.py
+++ mutmut/mutmut/node_mutation.py
@@ -46,6 +46,8 @@ def operator_string(
 
         for mut_func in supported_str_mutations:
             new_value = f"{prefix}{value[0]}{mut_func(value[1:-1])}{value[-1]}"
+            if "\\N" in new_value:
+                new_value = new_value.replace("\\N", "\\\\N")
             if new_value == value:
                 continue
             yield node.with_changes(value=new_value)
